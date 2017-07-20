88cda42795ebdb07af7c1dc99d5eb981aad80b7e
Colin Nichols <colinnic@gmail.com>
2017-07-20 15:07:18 -0600

    WIP: adding dropdown selector to replace tabs on smaller screens

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,35 @@
+function dropdownWithTabs(){
+	console.log('helloworld)');
+	$('.sync-with-tabs').each(function(){
+		var _this = $(this).attr('id');
+		var selected_dropdown = $('#'+_this).prev();
+		var selected_dropdown_text = $(selected_dropdown).find('.text');
+		var targeted_tabs = $('#'+_this).attr('data-sync-target');
+
+		$('#'+_this+ ' li').click(function(){
+			var selected_text = $(this).text();
+			$(selected_dropdown_text).text(selected_text);
+			
+			var selected_index = $(this).index();
+			$('#'+targeted_tabs+' li').eq(selected_index).find('a').click();
+			$('#'+_this).foundation('close');
+		});
+
+		$('#'+targeted_tabs+' li').click(function(){
+			var selected_text = $(this).text();
+			$(selected_dropdown_text).text(selected_text);
+		});
+
+
+		//sets current tab
+		var current_tab = $('#'+targeted_tabs+' li.is-active').text();
+		$(selected_dropdown_text).text(current_tab);
+	});
+}
+
+
 $(document).ready(function() {
   $(document).foundation();
+  dropdownWithTabs();
   //console.log('helloworld)');
 });
