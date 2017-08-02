5a42f0d9ad6d9e38dbb8b7b1d7bfc5cc87e8e49f
DevTech Systems <communications@devtechsys.com>
2017-08-02 13:44:40 -0400

    Updated App

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,9 +26,30 @@ function dropdownWithTabs(){
 	});
 }
 
+function getInternetExplorerVersion() {
+	// Returns the version of Internet Explorer or a -1 (indicating the use of another browser)
+  var rv = -1; // Return value assumes failure.
+  if (navigator.appName == 'Microsoft Internet Explorer')
+  {
+    var ua = navigator.userAgent;
+    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  return rv;
+}
+function checkVersion() {
+  var ver = getInternetExplorerVersion();
+  
+  if ( ver > -1 ) {
+    if ( ver < 11.0 ) {		
+      alert("Please note, this site was designed to work the best with\nInternet Explorer 11 or a newer EDGE browser.");
+		}
+  }
+}
 
 $(document).ready(function() {
   $(document).foundation();
   dropdownWithTabs();
-  //console.log('helloworld)');
+	checkVersion();
 });
