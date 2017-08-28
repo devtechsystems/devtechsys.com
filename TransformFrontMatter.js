e5a7582464df677ca79610e7c5767ccfe688d4b3
Andrew Blowe <ablowe@barquin.com>
2017-08-28 17:55:31 -0400

    added script to transform front matter in the format siteleaf needs

---
+++ TransformFrontMatter.js
@@ -0,0 +1,57 @@
+const fs = require('fs')
+const yaml = require('js-yaml')
+
+// SUMMARY:  Transform front matter by removing unnecessary fields,
+// and rearranging fields in a format that works well with siteleaf
+
+// Projects yaml transformation will:
+// 1.) expand the practice area string, and place it in a Yaml array
+// 2.) take the description field and move its contents to the 2nd document of this 
+// yaml document(Note: jekyll frontmatter is actually two yaml documents separated by ---)
+// 3.) Remove unnecessary fields we don't want to show up in siteleaf
+const projectsPath = '_projects/'
+fs.readdirSync(projectsPath).forEach((fileName) => {
+  const projectYaml = yaml.safeLoadAll(fs.readFileSync(projectsPath + fileName, 'utf8'))
+  const projectData = projectYaml[0]
+  console.log(`transforming practice areas for: ${projectsPath}${fileName}`)
+  const transformedData = transformPracticeAreas(projectData)
+  const projectDescription = transformedData['description'] // Description will later be appended to the document
+
+  // remove fields
+  const fieldsToDelete = [
+    'description' // will be included in content so it's not needed
+  ]
+  fieldsToDelete.forEach((fieldName) => {
+    delete transformedData[fieldName]
+  })
+  const transformedYaml = yaml.safeDump(transformedData)
+
+  console.log(`writing new yaml for: ${projectsPath}${fileName}`)
+  fs.writeFileSync(projectsPath + fileName, '---\n')
+  fs.appendFileSync(projectsPath + fileName, transformedYaml)
+  fs.appendFileSync(projectsPath + fileName, '---\n')
+  fs.appendFileSync(projectsPath + fileName, projectDescription)
+})
+
+// Publications yaml transformation will
+// 1.) expand the practice area string, and place it in a Yaml array
+const publicationsPath = '_publications/'
+fs.readdirSync(publicationsPath).forEach((fileName) => {
+  const publicationYaml = yaml.safeLoadAll(fs.readFileSync(publicationsPath + fileName, 'utf8'))
+  const publicationData = publicationYaml[0]
+  console.log(`transforming practice areas for: ${projectsPath}${fileName}`)
+  const transformedData = transformPracticeAreas(publicationData)
+  const transformedYaml = yaml.safeDump(transformedData)
+
+  console.log(`writing new yaml for: ${publicationsPath}${fileName}`)
+  fs.writeFileSync(publicationsPath + fileName, '---\n')
+  fs.appendFileSync(publicationsPath + fileName, transformedYaml)
+  fs.appendFileSync(publicationsPath + fileName, '---\n')
+})
+
+function transformPracticeAreas(project) {
+  const delimiter = '~'
+  const practiceAreasAsString = project['Practice Area'] || ''
+  const practiceAreasAsArray = practiceAreasAsString.split(delimiter)
+  return Object.assign({}, project, { 'Practice Area': practiceAreasAsArray })
+}
